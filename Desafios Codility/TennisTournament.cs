61df30929eb3c47565bac83cdfd536b85ebff7e5
BrunoBigelli <49571940+bbigelli@users.noreply.github.com>
2025-03-19 15:48:38 -0300

    Add files via upload
    TennisTournament Commit

---
+++ Desafios Codility/TennisTournament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Solution
+{
+    public int solution(int P, int C)
+    {
+        int maxGamesPLayers = P / 2;
+        int maxGamesCourts = C;
+
+        return Math.Min(maxGamesPLayers, maxGamesCourts);
+    }
+}
+
