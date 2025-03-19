0c526f6d2e1c328aeb9570314781cccf46caac7a
BrunoBigelli <49571940+bbigelli@users.noreply.github.com>
2025-03-19 15:35:00 -0300

    Add files via upload
    Primeiro Commit do primeiro desafio do Codility

---
+++ Desafios Codility/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Solution
+{
+    public int solution(int N)
+    {
+        string binaryRepresentation = Convert.ToString(N, 2);
+        int maxGap = 0;
+        int currentGap = 0;
+        bool counting = false;
+
+        foreach (char c in binaryRepresentation)
+        {
+            if (c == '1')
+            {
+                if (counting)
+                {
+                    maxGap = Math.Max(maxGap, currentGap);
+                    currentGap = 0;
+                }
+                counting = true;
+            }
+            else if (counting)
+            {
+                currentGap++;
+            }
+        }
+
+        return maxGap;
+    }
+}
